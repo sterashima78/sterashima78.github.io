8ddaebf025ea64ab0f37d9dddf5213357afb715d
sterashima <sterashima78@gmail.com>
2020-09-05 13:37:28 +0900

    top page

---
+++ src/layouts/default.amp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="$style.wrap">
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="scss" module>
+.wrap {
+  @apply min-h-screen;
+  @apply h-screen;
+}
+</style>
