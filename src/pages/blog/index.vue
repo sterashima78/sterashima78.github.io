b6480e792a91e157e1d76afa40be55ae9631c5fa
sterashima <sterashima78@gmail.com>
2021-01-12 21:02:27 +0900

    qiita

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -13,8 +13,26 @@ export default Vue.extend({
       .only(['title', 'path', 'description', 'tags', 'image', 'date'])
       .sortBy('date', 'desc')
       .fetch()
+    const qiitaPosts = await fetch(
+      'https://qiita.com/api/v2/items?query=user:sterashima78'
+    )
+      .then((res) => res.json())
+      .then((d) =>
+        d.map((d) => ({
+          type: 'qiita',
+          title: d.title,
+          url: d.url,
+          created: d.created_at,
+        }))
+      )
+    const newPosts = (Array.isArray(posts) ? posts : [posts])
+      .concat(qiitaPosts)
+      .sort(
+        (a, b) =>
+          Date.parse(b.date || b.created) - Date.parse(a.date || a.created)
+      )
     return {
-      posts,
+      posts: newPosts,
     }
   },
 })
